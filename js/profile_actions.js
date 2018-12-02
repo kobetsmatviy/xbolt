46d38bca5a0eb9f3f0c26452875b3458a83e0986
kobetsmatviy <kobetsmatviy@gmail.com>
2018-12-02 22:25:46 +0200

    made a block 'deletePost', style him and added some profile_actions.js

--- js/profile_actions.js
+++ js/profile_actions.js
@@ -11,4 +11,38 @@ $(document).ready(function(){
 
         $(".partCard").css('max-width', itemWidth);
     }
+
+    $('.withdraw').on('click', function() {
+        $(this).parent().append(
+            '<div class="deletePost">'+
+                '<span class="caption">Вам вдалося продати запчастину?!</span>'+
+                '<div class="vote">'+
+                    '<span class="yes">Так<span class="glyphicon glyphicon-thumbs-up"></span></span>'+
+                    '<span class="no">Ні<span class="glyphicon glyphicon-thumbs-down"></span></span>'+
+                '</div>'+
+            '</div>'+
+            '<div id="overlay"></div>');
+    });
+
+    $('.partCard').delegate('#overlay', 'click', function() {
+        $(this).remove();
+        $('.deletePost').remove();        
+    });
+
+    $('.partCard').delegate('.yes, .no', 'click', function() {
+        $(this).css({
+            'width': '100%',
+            'transition': 'all 1s'
+        });
+        $(this).next().css({
+            'width': '0',
+            'opacity': '0',
+            'transition': 'all 1s'
+        });
+        $(this).prev().css({
+            'width': '0',
+            'opacity': '0',
+            'transition': 'all 1s'
+        });
+    });
 });
