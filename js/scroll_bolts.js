d4d6d67424501dcd38a468ea4fc31c500e57e1b2
kobetsmatviy <kobetsmatviy@gmail.com>
2018-12-08 20:39:11 +0200

    worked by a style of #preview, added icons to #benefits, jquery animation of boltsSystem

---
+++ js/scroll_bolts.js
@@ -0,0 +1,6 @@
+﻿$(function () {
+    $(window).on('scroll', function() {
+        var deg = -($(window).scrollTop() / 5);
+        $('#preview .bolt').css({ transform: 'rotate(' + deg + 'deg)' });
+    }); 
+});
