2d78ccbadedc3415e26462700522db94da98701e
kobetsmatviy <kobetsmatviy@gmail.com>
2018-12-20 22:09:03 +0200

    realized social share blocks for fb, vk and tw

---
+++ js/share_socials.js
@@ -0,0 +1,10 @@
+﻿$(function () {
+    $('.share').on('click', function() {
+        $('.shareWindow').toggleClass('visible');
+    });
+    $(document).click(function(event) {
+        if ($(event.target).closest('.share').length) return;
+        $('.shareWindow').removeClass('visible');
+        event.stopPropagation();
+    });
+});
