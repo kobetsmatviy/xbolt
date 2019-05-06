42c90e6f0684912878355dcd3b02ad691e43970d
kobetsmatviy <kobetsmatviy@gmail.com>
2019-05-06 21:18:39 +0300

    THE HUGE MODIFIED OF PROFILE MENU AND HEADER AT ALL PAGES

--- js/header_actions.js
+++ js/header_actions.js
@@ -10,24 +10,11 @@ $(function () {
             market = false;
         }
     });
-
-    var profile = false;
-    $('#profileName').mousedown(function() {
-        if (profile == false) {
-            $('#profileNav').css('display', 'flex');
-            profile = true;
-        }
-        else if (profile == true) {
-            $('#profileNav').css('display', 'none');
-            profile = false;
-        }
-    });
     $(document).click(function(event) {
-        if ($(event.target).closest('#marketName, #profileName, #marketNav, #profileNav').length) return;
-        $('#marketNav, #profileNav').css('display', 'none');
+        if ($(event.target).closest('#marketName, #marketNav').length) return;
+        $('#marketNav').css('display', 'none');
 
         market = false;
-        profile = false;
         event.stopPropagation();
     });
 });
